2f221f02c4c9fb5ca5e0f7f11c966ecac8890842
eliserothberg <rothberg.elise@gmail.com>
2016-11-10 00:56:13 -0800

    more logic working and cleanup

--- app.js
+++ app.js
@@ -4,12 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-// var d3 = require("d3"),
-//     jsdom = require("jsdom");
-// var express = require('express');
-
-// var document = jsdom.jsdom(),
-//     svg = d3.select(document.body).append("svg");
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -20,8 +14,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
